4e20f2477b0e1d1f1212924a542ed30ebc1a5425
Unknown <nguyenhung9x2018@gmail.com>
2018-11-08 08:55:50 +0700

    zenfone3: rename

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo omni_Z017-userdebug
-add_lunch_combo omni_Z017-eng
-add_lunch_combo full_Z017-userdebug
-add_lunch_combo full_Z017-eng
+add_lunch_combo omni_zenfone3-userdebug
+add_lunch_combo omni_zenfone3-eng
+add_lunch_combo full_zenfone3-userdebug
+add_lunch_combo full_zenfone3-eng
